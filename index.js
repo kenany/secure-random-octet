58521118066cc0ccd49139fd441f41552d8e5f82
Kenan Yildirim <kenan@kenany.me>
2021-04-01 17:50:13 -0400

    fix: add JSDoc

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var getRandomValues = require('get-random-values');
 
+/**
+ * Generates a cryptographically secure octet.
+ *
+ * @returns {number} A cryptographically secure octet
+ */
 function secureRandomOctet() {
   var buf = new Uint8Array(1);
   getRandomValues(buf);
